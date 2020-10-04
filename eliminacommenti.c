d4aecb955324f7580509451d16dc69d1481537d8
tonyiovino <antonioiovino2004tony@gmail.com>
2020-10-04 17:54:57 +0200

    Elimina Commenti

--- eliminacommenti.c
+++ eliminacommenti.c
@@ -39,7 +39,7 @@ int main()
       }
     }
 
-    if (stato == SLASH){
+    else if (stato == SLASH){
       if (c == '*'){
         stato = COMMENTO;
       }
@@ -50,13 +50,13 @@ int main()
       }
     }
 
-    if (stato == COMMENTO){
+    else if (stato == COMMENTO){
       if (c == '*'){
         stato = ASTERISCO;
       }
     }
 
-    if (stato == ASTERISCO){
+    else if (stato == ASTERISCO){
       if (c == '/'){
         stato = NORM;
       }
